4eefe2d4fb92100e1615d4d27f4492697fd6c2ca
Regaip KURT <regkrt@gmail.com>
2020-11-09 09:56:19 -0100

    Add files via upload

---
+++ comment_finder.py
@@ -0,0 +1,65 @@
+#! /usr/bin/python3
+from bs4 import BeautifulSoup
+from bs4 import Comment
+import requests
+import argparse
+
+"""
+A simple python code to find commentlines on a webpage
+requirements --> bs4, lxml
+For install required libraries run this command: pip install bs4 lxml 
+"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", "--output", help="Write founded comments to an output file", required=False)
+parser.add_argument("-t", "--target", help="Webiste address to find comments inside its code", required=True)
+args = parser.parse_args()
+
+codename = """
+             _  _____  ____       ____   _    ____  ____     
+            | |/ / _ \|  _ \ _   |  _ \ / \  |  _ \/ ___|        
+            | ' / | | | | | (_)  | |_) / _ \ | |_) \___ \          
+            | . \ |_| | |_| |_   |  __/ ___ \|  _ < ___) |        
+            |_|\_\___/|____/(_)  |_| /_/   \_\_| \_\____/  
+"""
+
+finder = """
+  ____                                     _       _____ _           _      
+ / ___|___  _ __ ___  _ __ ___   ___ _ __ | |_    |  ___(_)_ __   __| | ___  _ __
+| |   / _ \| '_ ` _ \| '_ ` _ \ / _ \ '_ \| __|   | |_  | | '_ \ / _` |/ _ \| '__|
+| |__| (_) | | | | | | | | | | |  __/ | | | |_    |  _| | | | | | (_| |  __/| |
+ \____\___/|_| |_| |_|_| |_| |_|\___|_| |_|\__|   |_|   |_|_| |_|\__,_|\___||_|                      
+\n\n"""
+
+print(codename)
+print(finder)
+
+args.target = args.target if str(args.target).startswith("http") else "http://" + args.target 
+
+print("\nSearching comments on website: ", args.target)
+
+resp = requests.get(args.target)
+data = resp.text
+sp = BeautifulSoup(data, "lxml")
+comments = list(sp.findAll(text = lambda text: isinstance(text, Comment)))
+
+for i,j in enumerate(comments):
+        if j.replace(" ", "") == "":
+            comments.pop(i)
+
+if args.output != None:
+    with open(args.output, "w") as f:
+        for i in comments:
+            if i.replace(" ", "") == "":
+                continue
+            res = "*"*25 + "COMMENT STARTS" + "*"*23 + "\n" + i + "\n" + "*"*25 + "COMMENT ENDS" + "*"*25 + "\n" 
+            f.writelines(res)
+    print("Results have been written to file: ", args.output)
+else:
+    for i in comments:
+        if i.replace(" ", "") == "":
+            continue
+        res = "*"*25 + "COMMENT STARTS" + "*"*23 + "\n" + i + "\n" + "*"*25 + "COMMENT ENDS" + "*"*25 + "\n" 
+        print(res)
+
+print(str(len(comments)), " comments have founded!")
---
+++ kodbul.go
@@ -0,0 +1,78 @@
+package main
+
+// Bu program cheat.sh kullanarak aranan bir kod örneğini bulmaya yarar.
+// orjinal program: https://github.com/chubin/cheat.sh
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	if len(os.Args) <= 1 {
+		cmd := exec.Command("curl", "cht.sh")
+		res, err := cmd.Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(res))
+
+		cmd2 := exec.Command("tldr", "-h")
+		res, err = cmd2.Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(res))
+
+		fmt.Println("BU PROGRAM YUKARIDA GÖRÜLEN cht.sh ve tldr PROGRAMLARI ÜZERİNDEN ARAMA YAPAR.\ntldr komutunun yüklenmiş olması gerekir.")
+		fmt.Println("En az bir programlama dili veya parametre belirtin!")
+		fmt.Println("Kullanım (1): kodbul <programlama dili> <aranacak ifadeler>")
+		fmt.Println("Kullanım (2): kodbul <bash girdisi>")
+
+		os.Exit(0)
+	}
+
+	var args string
+	var url string
+
+	if len(os.Args) == 2 {
+		if os.Args[1] != "-h" {
+			cmd := exec.Command("tldr", os.Args[1])
+			res, err := cmd.Output()
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(string(res))
+		} else {
+			fmt.Println("BU PROGRAM cht.sh ve tldr PROGRAMLARI ÜZERİNDEN ARAMA YAPAR.\ntldr komutunun yüklenmiş olması gerekir.")
+			fmt.Println("\nKullanım (1): kodbul <programlama dili> <aranacak ifadeler>")
+			fmt.Println("Kullanım (2): kodbul <bash girdisi>")
+			os.Exit(0)
+		}
+
+	}
+
+	for _, w := range os.Args[2:] {
+		args += w + " "
+	}
+
+	args, url = "", ""
+
+	for _, w := range os.Args[2:] {
+		args += w + "+"
+	}
+
+	if len(args) > 0 {
+		url = "cht.sh/" + os.Args[1] + "/" + args[0:len(args)-1]
+	} else {
+		url = "cht.sh/" + os.Args[1]
+	}
+
+	cmd := exec.Command("curl", url)
+	res, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(res))
+}
